0f0a8f066da8bf69b0a354e7744dabcafbf2feb0
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2022-12-27 19:58:54 +0100

    counter added

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,9 +1,24 @@
-import { useEffect, useState, useLayoutEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Timer = () => {
+
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    let intervalId = null;
+
+    intervalId = window.setInterval(() => {
+      setTimer(prev => prev + 1);
+    }, 1000);
+
+    return () => {
+      window.clearInterval(intervalId)
+    }
+  }, [])
+
   return(
     <>
-      <span>TIMER</span>
+      <span>{timer} s</span>
     </>
   )
 }
